2a449a5fcd05e1221a0cff86ac66bdaabeac8d7c
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-12-01 14:28:41 +0500

    add firebase config

---
+++ Firebase/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3xqO4x3KBXM2bEJFLes0CcJ-5SZKG6iU",
+    authDomain: "signal-chat-app-66f47.firebaseapp.com",
+    projectId: "signal-chat-app-66f47",
+    storageBucket: "signal-chat-app-66f47.appspot.com",
+    messagingSenderId: "133227142291",
+    appId: "1:133227142291:web:bdc65c10dc8cf36aae8166"
+  };
